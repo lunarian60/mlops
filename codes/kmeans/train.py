9eacd239821985c2e59e77f476444bc1f8abeb70
Lee <leeyudon@88665a2791f3.ant.amazon.com>
2021-12-25 15:27:00 +0900

    modified kmeans script

--- codes/kmeans/train.py
+++ codes/kmeans/train.py
@@ -8,13 +8,104 @@ import numpy
 # import matplotlib.pyplot as plt
 import sys
 # sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
+# from feature_format import featureFormat, targetFeatureSplit
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
 
 import argparse
 import os
+import numpy as np
 
+
+def featureFormat( dictionary, features, remove_NaN=True, remove_all_zeroes=True, remove_any_zeroes=False, sort_keys = False):
+    """ convert dictionary to numpy array of features
+        remove_NaN = True will convert "NaN" string to 0.0
+        remove_all_zeroes = True will omit any data points for which
+            all the features you seek are 0.0
+        remove_any_zeroes = True will omit any data points for which
+            any of the features you seek are 0.0
+        sort_keys = True sorts keys by alphabetical order. Setting the value as
+            a string opens the corresponding pickle file with a preset key
+            order (this is used for Python 3 compatibility, and sort_keys
+            should be left as False for the course mini-projects).
+        NOTE: first feature is assumed to be 'poi' and is not checked for
+            removal for zero or missing values.
+    """
+
+
+    return_list = []
+
+    # Key order - first branch is for Python 3 compatibility on mini-projects,
+    # second branch is for compatibility on final project.
+    if isinstance(sort_keys, str):
+        import pickle
+        keys = pickle.load(open(sort_keys, "rb"))
+    elif sort_keys:
+        keys = sorted(dictionary.keys())
+    else:
+        keys = dictionary.keys()
+
+    for key in keys:
+        tmp_list = []
+        for feature in features:
+            try:
+                dictionary[key][feature]
+            except KeyError:
+                print("error: key ", feature, " not present")
+                return
+            value = dictionary[key][feature]
+            if value=="NaN" and remove_NaN:
+                value = 0
+            tmp_list.append( float(value) )
+
+        # Logic for deciding whether or not to add the data point.
+        append = True
+        # exclude 'poi' class as criteria.
+        if features[0] == 'poi':
+            test_list = tmp_list[1:]
+        else:
+            test_list = tmp_list
+        ### if all features are zero and you want to remove
+        ### data points that are all zero, do that here
+        if remove_all_zeroes:
+            append = False
+            for item in test_list:
+                if item != 0 and item != "NaN":
+                    append = True
+                    break
+        ### if any features for a given data point are zero
+        ### and you want to remove data points with any zeroes,
+        ### handle that here
+        if remove_any_zeroes:
+            if 0 in test_list or "NaN" in test_list:
+                append = False
+        ### Append the data point if flagged for addition.
+        if append:
+            return_list.append( np.array(tmp_list) )
+
+    return np.array(return_list)
+
+
+def targetFeatureSplit( data ):
+    """
+        given a numpy array like the one returned from
+        featureFormat, separate out the first feature
+        and put it into its own list (this should be the
+        quantity you want to predict)
+
+        return targets and features as separate lists
+
+        (sklearn can generally handle both lists and numpy arrays as
+        input formats when training/predicting)
+    """
+
+    target = []
+    features = []
+    for item in data:
+        target.append( item[0] )
+        features.append( item[1:] )
+
+    return target, features
 
 
 # def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
